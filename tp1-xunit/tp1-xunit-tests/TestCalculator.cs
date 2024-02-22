82b0d9383e94b3036260a8cf4b08fc5022645d47
Henrique Andrade Cruz <henrique.andrade22@hotmail.com>
2024-02-22 14:34:22 -0300

    update: .NET version to 8.0

--- tp1-xunit/tp1-xunit-tests/TestCalculator.cs
+++ tp1-xunit/tp1-xunit-tests/TestCalculator.cs
@@ -1,46 +1,45 @@
 using tp1_xunit;
 
-namespace tp1_xunit_tests
+namespace tp1_xunit_tests;
+
+public class TestCalculator
 {
-    public class TestCalculator
+    [Fact]
+    public void AddTest()
     {
-        [Fact]
-        public void AddTest()
-        {
-            Assert.Equal(11, Calculator.Add(5, 6));
-            Assert.Equal(31.3, Calculator.Add(15.1, 16.2));
-        }
+        Assert.Equal(11, Calculator.Add(5, 6));
+        Assert.Equal(31.3, Calculator.Add(15.1, 16.2));
+    }
 
-        [Fact]
-        public void SubTest()
-        {
-            Assert.Equal(6, Calculator.Sub(11, 5));
-            Assert.Equal(-3, Calculator.Sub(5, 8));
-            Assert.Equal(13.1, Calculator.Sub(20.1, 7));
-        }
+    [Fact]
+    public void SubTest()
+    {
+        Assert.Equal(6, Calculator.Sub(11, 5));
+        Assert.Equal(-3, Calculator.Sub(5, 8));
+        Assert.Equal(13.1, Calculator.Sub(20.1, 7));
+    }
 
-        [Fact]
-        public void MulTest()
-        {
-            Assert.Equal(12, Calculator.Mul(3, 4));
-            Assert.Equal(6.6, Calculator.Mul(3.3, 2));
-        }
+    [Fact]
+    public void MulTest()
+    {
+        Assert.Equal(12, Calculator.Mul(3, 4));
+        Assert.Equal(6.6, Calculator.Mul(3.3, 2));
+    }
 
-        [Fact]
-        public void DivTest()
-        {
-            Assert.Equal(7, Calculator.Div(21, 3));
-            Assert.Equal(2.5, Calculator.Div(5, 2));
-        }
+    [Fact]
+    public void DivTest()
+    {
+        Assert.Equal(7, Calculator.Div(21, 3));
+        Assert.Equal(2.5, Calculator.Div(5, 2));
+    }
 
-        [Fact]
-        public void DivByZeroTest()
-        {
-            static void act() => Calculator.Div(5, 0);
+    [Fact]
+    public void DivByZeroTest()
+    {
+        static void act() => Calculator.Div(5, 0);
 
-            var ex = Assert.Throws<DivideByZeroException>(act);
+        var ex = Assert.Throws<DivideByZeroException>(act);
 
-            Assert.Equal("Attempted to divide by zero.", ex.Message);
-        }
+        Assert.Equal("Attempted to divide by zero.", ex.Message);
     }
 }
--- tp1-xunit/tp1-xunit/Calculator.cs
+++ tp1-xunit/tp1-xunit/Calculator.cs
@@ -1,15 +1,14 @@
-﻿namespace tp1_xunit
+﻿namespace tp1_xunit;
+
+public static class Calculator
 {
-    public static class Calculator
-    {
-        public static double Add(double x, double y) => Math.Round(x + y, 2);
+    public static double Add(double x, double y) => Math.Round(x + y, 2);
 
-        public static double Sub(double x, double y) => Math.Round(x - y, 2);
+    public static double Sub(double x, double y) => Math.Round(x - y, 2);
 
-        public static double Mul(double x, double y) => Math.Round(x * y, 2);
+    public static double Mul(double x, double y) => Math.Round(x * y, 2);
 
-        public static double Div(double x, double y) => y != 0 ?
-            Math.Round(x / y, 2) :
-            throw new DivideByZeroException();
-    }
+    public static double Div(double x, double y) => y != 0 ?
+        Math.Round(x / y, 2) :
+        throw new DivideByZeroException();
 }
